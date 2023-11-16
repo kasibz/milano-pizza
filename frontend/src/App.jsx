ec4e8784572b57ad294d0d1e0116c228ab435aff
Ramroop <alyssa.ramroop@fiserv.com>
2023-11-16 15:49:18 -0500

    customer-lookup-undone

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from './pages/HomePage';
 import POSPage from './pages/POSPage';
 import EmployeeLogin from './pages/EmployeeLogin';
 import EmployeeSignup from './pages/EmployeeSignup';
+import CustomerLookup from './pages/CustomerLookup';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/pos" element = {<POSPage/>} />
           <Route path="/login" element = { <EmployeeLogin/> } />
           <Route path="/signup" element = { <EmployeeSignup/> } />
+          <Route path="/customerlookup" element = { <CustomerLookup /> }/>
         </Routes>
       </Router>
     </>
