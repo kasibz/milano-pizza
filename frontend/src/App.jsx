783baabb80be404816696623bf94967644cb9f6d
An <junhan.an@fiserv.com>
2023-11-21 12:57:32 -0500

    employee detail page complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import EmployeeSignup from './pages/EmployeeSignup';
 import EmployeeLogOut from './pages/EmployeeLogOut';
 import AddProduct from './pages/AddProduct';
 import OrderZipcode from './pages/OrderZipcode';
+import EmployeeOrderDetail from './pages/EmployeeOrderDetail';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route path="/logout" element = { <EmployeeLogOut/> } />
           <Route path='/addProduct' element = { <AddProduct/> } />
           <Route path='/orderbyzipcode/:zipcodeID' element = { <OrderZipcode/> } />
+          <Route path='/employeeorderdetail' element = { <EmployeeOrderDetail/> } />
         </Routes>
       </Router>
     </>
