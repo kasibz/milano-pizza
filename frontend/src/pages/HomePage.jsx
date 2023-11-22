3bd7cae313d33eabc2dec2d45a823e3192941f0b
kasib <kasibzxc@gmail.com>
2023-11-22 13:24:03 -0500

    added favicon

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -10,7 +10,6 @@ const HomePage = () => {
     const [zip_code, setZip_Code] = useState('');
     
     const navigate = useNavigate();
-    // if get is not none localStorage.setItem('loggedInEmployee', JSON.stringify(employeeData));
     const zipcodes = [55501, 55502, 55503, 55504]
 
     const handleZipCodeLookUp = (event) => {
@@ -45,29 +44,16 @@ const HomePage = () => {
         navigate('/login');
         }
     },[navigate]);
-
-    // useEffect(() => {
-    //     axios.get()
-    // })
-
-    
-    // console.log(localStorage.getItem('loggedInEmployee'))
     
     return (
         <MainLayout>
             <div className='bg-light p-5 mt-4 rounded-3'>
                 <h1>Welcome to Alyssa Milano's Pizzaria POS system</h1>
                 <p></p>
-                <Link to='/pos' className='btn btn-primary'>Click to start the POS system</Link><br /><br />
+                {/* <Link to='/pos' className='btn btn-primary'>Click to start the POS system</Link><br /><br /> */}
                 <Link to='/employeeorderdetail' className='btn btn-primary'>Orders By Employee</Link>
                 <br /><br />
-                {/* {
-                    zipcodes.map((zipcodeID, idx) => {
-                        return (
-                        < Link key={idx} to={`/orderbyzipcode/${zipcodeID}`} className='btn btn-primary'>Orders By {zipcodeID}</Link>
-                    )
-                    })
-                } */}
+
                 <label className='dropdown'>View Orders by ZipCode</label>
                 <select 
                     id='lookUpByZip' 
@@ -84,8 +70,8 @@ const HomePage = () => {
                 </select>
             </div>
             <div className="offset-2 col-4">
-                <div className="shadow p-4 mt-4">
-                    <h2>New Customer</h2>
+                <div id="customerinputbox" className="shadow p-4 mt-4" >
+                    <h2>Enter Customer to Start Order</h2>
                     <form onSubmit={handleNewCustomer}>
                         <div className="form-floating mb-3">
                             <input
