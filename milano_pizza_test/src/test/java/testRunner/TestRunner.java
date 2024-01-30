220eadb2746dcf6aba33cc4123ae64d173003974
Kasib <kasibabdullah8@gmail.com>
2024-01-30 09:20:47 -0500

    added testRunner

---
+++ milano_pizza_test/src/test/java/testRunner/TestRunner.java
@@ -0,0 +1,17 @@
+package testRunner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "/Users/kasibabdullah/Documents/Computer_Science/repos/milano-pizza/milano_pizza_test/Features/Pizza.feature",
+        glue = "stepDefinitions"
+)
+
+
+public class TestRunner {
+    
+}
