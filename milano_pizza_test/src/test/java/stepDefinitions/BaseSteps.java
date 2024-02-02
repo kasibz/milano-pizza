21a0d15fe951a2d55afd45f49a6d8ab288b3c6c9
Kasib Abdullah <kasib.abdullah@fiserv.com>
2024-02-02 10:13:12 -0500

    added functions for creating customers

--- milano_pizza_test/src/test/java/stepDefinitions/BaseSteps.java
+++ milano_pizza_test/src/test/java/stepDefinitions/BaseSteps.java
@@ -1,5 +1,6 @@
 package stepDefinitions;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.WebDriver;
 import pageObjects.HomePage;
 import pageObjects.LoginPage;
@@ -9,4 +10,16 @@ public class BaseSteps {
     public WebDriver driver;
     public LoginPage lp;
     public HomePage hp;
+
+
+    public static String generateAddress() {
+        String streetNumber = RandomStringUtils.randomNumeric(3);
+        String streetName = RandomStringUtils.randomAlphabetic(5);
+        return String.format("%s %s", streetNumber, streetName);
+    }
+
+    public static String generatePhoneNumber() {
+        return RandomStringUtils.randomNumeric(10);
+    }
+
 }
